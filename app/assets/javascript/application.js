bd69282366ab02b8edfa7a9e660fe10387e58d56
andrewaclark248 <andrewaclark248@gmail.com>
2022-07-29 15:39:32 -0400

    add js

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-  $(".add-url").click(function(){
-    alert("some adta alert");
-  });
+
+	var input = "<div class='form-group col-12 input-copy'><label>Url</label><input class='form-control'></input></div>"
+
+	$(".add-url").click(function(){
+		$( ".user-password-form" ).append(input);
+	});
+	$(".remove-url").click(function(){
+		//$(".input-copy").last().forEach(myFunction);
+		$(".input-copy").last().remove();
+		//var inputLength = $(".input-copy").length
+		//for (let i = 0; i < inputLength; i++) {
+		//}
+		//console.log($(".input-copy")[0].remove());
+	});
+
 });
+
