bb4f4b4339e01a70b8dd520fa01c483d01158ab5
andrewaclark248 <55842531+andrewaclark248@users.noreply.github.com>
2022-07-31 14:29:50 -0400

    add username to password (#5)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_135025) do
+ActiveRecord::Schema.define(version: 2022_07_31_182033) do
 
   create_table "logins", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_07_28_135025) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["user_id"], name: "index_passwords_on_user_id"
   end
 
