e57a500cf369ce49d57184563bb305cd3ef45a86
andrewaclark248 <andrewaclark248@gmail.com>
2022-08-22 21:41:48 -0400

    return creds

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,13 +1,20 @@
 module Api
-  class ApplicationController < ActionController::Base
+  class  Api::V1::ApplicationController < ActionController::Base
 
   	before_action :authenticate
 
 
   	def authenticate
-		@current_login = Login.find_by(auth_token: api_params[:auth_token])
+		auth_token = request.headers['Authorization'].split(" ").second
+
+		if auth_token.nil?
+			render json: {error: "Invalid Login"}
+			return
+		end
+
+		@current_login = Login.find_by(auth_token: auth_token)
 		if @current_login.nil?
-			render json {error: "Invalid Login"}
+			render json: {error: "Invalid Login"}
 		end
   	end
 
