82b5c6f7c84ff413bdec27267550a62c46bc462f
andrewaclark248 <andrewaclark248@gmail.com>
2022-09-24 21:52:30 -0400

    variosu changnes

--- app/controllers/user_passwords_controller.rb
+++ app/controllers/user_passwords_controller.rb
@@ -27,6 +27,7 @@ class UserPasswordsController < ApplicationController
 		@password = Password.find_by(id: params[:id])
 		@rotate_password_text = "The 'Manually Rotate Password' button rotates your current password."
 		@share_password_text = "Share your phone number with a contact!"
+		render "edit2"
 	end
 
 	def update
