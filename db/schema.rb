983fc149d61b7a046a7c4aeea86ead07242f695e
andrewaclark248 <55842531+andrewaclark248@users.noreply.github.com>
2022-10-11 22:24:01 -0400

    password stength (#55)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_08_234431) do
+ActiveRecord::Schema.define(version: 2022_10_12_014643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_10_08_234431) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "pwd_strength"
     t.index ["user_id"], name: "index_passwords_on_user_id"
   end
 
