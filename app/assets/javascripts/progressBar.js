1462a9bdd6f8614442904c617a77e6d11b712c95
andrewaclark248 <andrewaclark248@gmail.com>
2022-10-12 22:17:29 -0400

    init dashboard

---
+++ app/assets/javascripts/progressBar.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    console.log("asdfasdf")
+    $(".progress").each(function() {
+
+        var value = $(this).attr('data-value');
+        var left = $(this).find('.progress-left .progress-bar');
+        var right = $(this).find('.progress-right .progress-bar');
+
+        if (100 > 0) {
+        if (value <= 50) {
+            right.css('transform', 'rotate(' + percentageToDegrees(80) + 'deg)')
+        } else {
+            right.css('transform', 'rotate(180deg)')
+            left.css('transform', 'rotate(' + percentageToDegrees(80 - 50) + 'deg)')
+        }
+        }
+
+    })
+});
+console.log("asdfasdf")
+
+function percentageToDegrees(percentage) {
+
+    return percentage / 100 * 360
+
+}
