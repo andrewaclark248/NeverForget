60da1b541850ed45d53bcac04a90c43340da18c1
andrewaclark248 <andrewaclark248@gmail.com>
2022-10-16 12:35:00 -0400

    various style changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,7 +36,7 @@ module ApplicationHelper
     end
 
     def logo_text
-        "<h3><span class='text-dark'>Safe</span><span class='text-dark'>Logins</span></h3>".html_safe
+        "<h3><span class='text-dark'>Safe</span><span class='text-danger'>Logins</span></h3>".html_safe
     end
 
     def striped_table index
@@ -48,7 +48,7 @@ module ApplicationHelper
     end
 
     def add_url_btn
-        "<div><i class='fa-solid fa-plus pe-2 text-dark'><span>Add U R L</span></div>".html_safe
+        "<div><i class='fa-solid fa-plus pe-2'></i><span class=''>Add URL</span></div>".html_safe
     end
 
     def get_pwd_color pwd_strength
