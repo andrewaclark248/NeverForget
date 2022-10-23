ddd18e543566a09b3fab79b6329fcb4653921540
andrewaclark248 <55842531+andrewaclark248@users.noreply.github.com>
2022-10-22 21:58:49 -0400

    Profile Add Password (#73)
    * send test email
    
    * change password

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -42,4 +42,58 @@ $(document).ready(function(){
     $('#gold_mfa_phone').mask('(999)-999-9999'); 
 
 
+
+    $(".sendTestEmail").click(function(e){
+        $(".bronze_email").val()
+        var email = null
+        if ($("#bronze_email").val() != undefined) {
+            email = $("#bronze_email").val() 
+        } else if ($("#platnium_email").val() != undefined) {
+            email = $("#platnium_email").val() 
+        }
+        sendTestEmail(email)
+
+    })
+
+
+    //eyeIconCursorProfile
+
+    $(".eyeIconCursorProfile").on('click',function() {
+        if ($("[id$='password']").attr('type') === 'password') {
+            $(".eyeIconCursorProfile").removeClass("fa-eye")
+            $(".eyeIconCursorProfile").addClass("fa-eye-slash")
+            $("[id$='password']").attr('type', 'text');
+        } else {
+            $(".eyeIconCursorProfile").removeClass("fa-eye-slash")
+            $(".eyeIconCursorProfile").addClass("fa-eye")
+            $("[id$='password']").attr('type', 'password');
+        }
+    });
+    $("[id$='confirm_password']")
+
+    $(".eyeIconCursorProfileConfirm").on('click',function() {
+        if ($("[id$='password_confirmation']").attr('type') === 'password') {
+            $(".eyeIconCursorProfileConfirm").removeClass("fa-eye")
+            $(".eyeIconCursorProfileConfirm").addClass("fa-eye-slash")
+            $("[id$='password_confirmation']").attr('type', 'text');
+        } else {
+            $(".eyeIconCursorProfileConfirm").removeClass("fa-eye-slash")
+            $(".eyeIconCursorProfileConfirm").addClass("fa-eye")
+            $("[id$='password_confirmation']").attr('type', 'password');
+        }
+    });
+
+
+
+    
 });
+
+function sendTestEmail(email) {
+    var url = '/ajax/send_test_email/'
+    $.ajax({
+        method: "POST",
+        url: url,
+        data: { email: email }
+    })
+
+}
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 	get '/ajax/get_new_password', to: 'ajax#get_new_password'
 	get 'ajax/get_password/:id', to: 'ajax#get_password'
 	get 'ajax/get_key/:id', to: 'ajax#get_key'
+	post '/ajax/send_test_email', to: "ajax#send_test_email"
 
 	#CRUD controllers
 	resources :dashboard
@@ -31,7 +32,7 @@ Rails.application.routes.draw do
 		member do
 			post 'update_login', to: "profiles#update_login"
 			post 'configure_mfa', to: "profiles#configure_mfa"
-
+			post 'reset_password', to: "profiles#reset_password"
 		end
 	end
 	resources :phones do
