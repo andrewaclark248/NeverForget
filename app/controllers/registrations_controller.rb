f83d4c9e6f99ea8a546b49f0c2e7ff05c9ff8946
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-31 22:41:55 -0500

    update callback

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -21,6 +21,6 @@ class RegistrationsController < Devise::RegistrationsController
     private
 
     def after_sign_up_path_for(resource)
-        new_login_registration_path    
+        root_path    
     end
 end
