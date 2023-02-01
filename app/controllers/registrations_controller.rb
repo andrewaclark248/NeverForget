6c3d7d1062bf992b1a9531183b647bb9e3016cc0
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-31 21:51:05 -0500

    add redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
 
     def new
         super
+        redirect_to new_login_session_path
     end
     
     def configure_permitted_parameters
