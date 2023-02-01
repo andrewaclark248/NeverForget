b870ed6e2ab2b0d2a297b25128e8f225f82676c4
andrewaclark248 <andrewaclark248@gmail.com>
2023-01-31 23:24:54 -0500

    plural paht

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 	#  }
   	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-	get '/login', to: 'home#index'
+	get '/logins', to: 'home#index'
 
 	#ajax request
 	get '/ajax/get_new_password', to: 'ajax#get_new_password'
