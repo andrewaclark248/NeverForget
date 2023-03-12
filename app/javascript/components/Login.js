080f2f6e963d085472b0fbc434ed606e5cd1995d
andrewaclark248 <andrewaclark248@gmail.com>
2023-03-11 19:17:37 -0500

    update stuff

---
+++ app/javascript/components/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class Login extends Component {
+  render(){
+    return(
+      <div style={{top: 100, left: 100, color: "black"}}> 
+        Some text displayed
+      </div>
+
+    )
+  }
+}
+
+
+export default Login
