34acb390c86137ffa9d7d10979ab2448993fa992
andrewaclark248 <andrewaclark248@gmail.com>
2023-04-23 12:09:40 -0400

    various changes

--- app/javascript/components/CustomStyle.js
+++ app/javascript/components/CustomStyle.js
@@ -4,7 +4,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 export const theme = createTheme({
     palette: {
       primary: {
-        main: "#d32f2f"
+        main: "#DB3444"
       },
       secondary: {
         main: "#0288d1"
--- app/javascript/components/KeyValue.js
+++ app/javascript/components/KeyValue.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import {Box, Typography, Button, Grid, TablePagination, CardContent, Card, CardHeader } from '@mui/material';
+import {Box, Typography, Button, Grid, TablePagination, CardContent, Card, CardHeader,
+    FormControl, InputLabel, Select, MenuItem, TextField } from '@mui/material';
 import "./reactStyles.css"
 
 function KeyValue() {
@@ -24,11 +25,10 @@ function KeyValue() {
                     md={12}
                     lg={12}
                 >
-                    <Typography variant="h4" sx={{fontWeight: 'medium'}}>
-                        Create a Key
-                    </Typography>
+                        <Typography variant="h5" sx={{fontWeight: 'bold'}}>
+                            New Key
+                        </Typography>
                 </Grid>
-
 
 
                 <Grid
@@ -46,17 +46,50 @@ function KeyValue() {
                         <table style={{width: "100%", paddingBottom: 0, marginBottom:0}} className="table">
 
                             <tbody>
-                            <tr style={{width: "100%"}} className="safe-card-row">
-                                <td className="cell">Emil</td>
-                                <td className="cell">Tobias</td>
+                            <tr style={{width: "100%", height: "80px"}} className="safe-card-row">
+                                <td className="cell column-label" style={{verticalAlign: "middle"}} >
+                                    <span  >
+                                    <center>
+                                    <Typography variant="h6">Key</Typography>
+                                    </center>
+                                    </span>
+
+                                </td>
+                                <td className="cell column-value" style={{verticalAlign: "middle"}} >
+                                    <center>
+                                        <FormControl size="small" sx={{width: "60%"}}>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                            >
+                                                <MenuItem value={10}>SSN</MenuItem>
+                                                <MenuItem value={20}>Bank PIN Code</MenuItem>
+                                                <MenuItem value={30}>WIFI Password</MenuItem>
+                                                <MenuItem value={30}>Custom Label (Create your own lable)</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                    </center>
+
+
+                                </td>
                             </tr>
-                            <tr className="safe-card-row">
-                                <td className="cell">16</td>
-                                <td className="cell">14</td>
-                            </tr>
-                            <tr className="safe-card-row">
-                                <td className="cell">16</td>
-                                <td className="cell">10</td>
+                            <tr className="safe-card-row" style={{width: "100%", height: "80px"}} >
+                                <td className="cell column-label" style={{verticalAlign: "middle"}}>
+                                    <center>
+                                    <Typography variant="h6">Value</Typography>
+                                    </center>
+                                </td>
+                                <td className="cell column-value" style={{verticalAlign: "middle"}}>
+                                    <center>
+                                        <TextField
+                                            id="filled-size-small"
+                                            defaultValue="Small"
+                                            size="small"
+                                            sx={{width: "60%"}}
+                                        />
+                                    </center>
+
+                                </td>
                             </tr>
                             </tbody>
 
@@ -70,6 +103,18 @@ function KeyValue() {
 
 
                 </Grid>
+
+                <Grid
+                    item
+                    xs={12}
+                    md={12}
+                    lg={12}
+                >
+                    <Button variant="contained" fullWidth sx={{fontWeight: "medium"}}>Create Key</Button>
+
+                </Grid>
+
+
 
             </Grid>
         </Box>
