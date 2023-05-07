d7e4dbe9103a3eb926d5728c61b0b398326622d2
andrewaclark248 <andrewaclark248@gmail.com>
2023-05-07 10:13:03 -0400

    fix navbar

--- app/assets/javascripts/navBar.js
+++ app/assets/javascripts/navBar.js
@@ -7,35 +7,7 @@ console.log("file laoded haha")
 
 $(document).ready(function(){
 
-    //on hover change color 
-    $(".navbarIconColorChange").hover(function(e){
-        let parentElm = e.target?.parentElement?.tagName;
-        let elmTag = e.target.tagName;
-        
-        if (parentElm == "svg") {
-            //$(e.target.parentElement).attr("fill","white");
-        } else if (elmTag == "svg" ){ 
-            //$(e.target).attr("fill","white");
-        } else if (parentElm == "span" || parentElm == "SPAN") {
-            //$(e.target?.parentElement).removeClass("navBarIconTextChangeGrey");
-            //$(e.target?.parentElement).addClass("navBarIconTextChangeWhite");
-            
-        }
-        }, function(e){
-            let parentElm = e.target?.parentElement?.tagName;
-            let elmTag = e.target.tagName;
 
-            if (parentElm == "svg") {
-                //$(e.target.parentElement).attr("fill","#C0C0C0");
-            } else if (elmTag == "svg" ){ 
-                //$(e.target).attr("fill","#C0C0C0");
-            } else if (parentElm == "span" || parentElm == "SPAN") {
-                //$(e.target?.parentElement).removeClass("navBarIconTextChangeWhite");
-                //$(e.target?.parentElement).addClass("navBarIconTextChangeGrey");
-                
-            }
-            
-        });
 
         //
         $(".expandNavbar").hover(function(e){
@@ -44,10 +16,13 @@ $(document).ready(function(){
 
             let tagName = e.target?.tagName;
             let parentTagName = e.target?.parentElement.tagName;
-
+            console.log("tagName", tagName)
+            console.log("parentTagName", parentTagName == "A")
             if (tagName == "li" || tagName == "LI") {
                 $(e.target).addClass("expandNavbarHover");
             } else if ( parentTagName == "li" || parentTagName == "LI" ) {
+                $(e.target.parentElement).addClass("expandNavbarHover");
+            } else if (parentTagName == "A" || parentTagName == "a") {
                 $(e.target.parentElement).addClass("expandNavbarHover");
             }
 
@@ -55,11 +30,12 @@ $(document).ready(function(){
             }, function(e){
                 let tagName = e.target?.tagName;
                 let parentTagName = e.target?.parentElement.tagName;
-                console.log("tagName", tagName)
-                console.log("parentTagName", parentTagName)
+
                 if (tagName == "li" || tagName ==  "LI") {
                     $(e.target).removeClass("expandNavbarHover");
                 } else if ( parentTagName == "li" || parentTagName == "LI" ) {
+                    $(e.target.parentElement).removeClass("expandNavbarHover");
+                }else if (parentTagName == "A" || parentTagName == "a") {
                     $(e.target.parentElement).removeClass("expandNavbarHover");
                 }
 
@@ -67,7 +43,7 @@ $(document).ready(function(){
 
 
     
-    //on change
+    //change width of navbar
     $("#expandNavbar").click(function(){
 
         let isSmallNavbar = $(".sidenav").hasClass("navBarSmall")
